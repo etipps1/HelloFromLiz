c1117c11cbc882d8190536585a26d9d622ea5f99
etipps1 <77982378+etipps1@users.noreply.github.com>
2021-01-29 12:00:51 -0600

    Uploading HelloFromLiz files
    Remade repository due to bad naming choice

---
+++ HelloFromLiz/src/tippetts/HeathersHello.java
@@ -0,0 +1,10 @@
+package tippetts;
+
+public class HeathersHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Heather");
+
+        System.out.println("Hello Heather, my name is Liz");
+    }
+}
